1bac5f22dd8c82a77583276903d7be0f61c6050d
unknown <181034>
2021-11-18 11:04:27 +0900

    first

---
+++ src/test/java/com/example/skywaydemo/SkywayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skywaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkywayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
